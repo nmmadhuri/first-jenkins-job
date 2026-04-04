26ad168b8a0d700991c53d7f3b40c2b04642da49
Madhuri Kurapati <madhurikurapati@Madhuris-MacBook-Protada.local>
2026-04-03 22:46:41 -0700

    Jenkins commit `

---
+++ playwright.config.js
@@ -0,0 +1,85 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: 1,
+  /* Opt out of parallel tests on CI. */
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://the-internet-5chk.onrender.com',
+    headless: false,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'retain-on-first-failure',
+    screenshot: 'on-first-failure',
+    video: 'on-first-retry'
+   // video: {mode: 'retain-on-failure', size: {width: 1920, height: 1080}}
+  
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+
+    {
+      name: 'Google Chrome',
+      use: { 
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        viewport: {width: 1900, height: 1080} 
+       },
+    },
+    
+    
+   
+  /* {
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport: {width: 1900, height: 1080} 
+       },
+    },
+
+   {
+      name: 'firefox',
+      use: { 
+        ...devices['Desktop Firefox'],
+        viewport: {width: 1850, height: 1000} 
+       },
+    },
+
+    {
+      name: 'webkit',
+      use: { 
+        ...devices['Desktop Safari'],
+        viewport: {width: 1850, height: 1000}  
+      },
+    },
+*/
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+  ],
+
+});
+
