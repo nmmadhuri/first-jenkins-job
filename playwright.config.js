b6c14e64182073c64ef59334fd605c185d6e024d
Madhuri <nmmadhuri@gmail.com>
2026-04-13 16:13:00 -0700

    can run parallely

--- playwright.config.js
+++ playwright.config.js
@@ -7,13 +7,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -82,4 +82,3 @@ export default defineConfig({
   ],
 
 });
-
